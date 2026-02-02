a5ef09bdfe4a0137d1f29782ca058c937dd27881
Francisco Santarelli <franciscosantarelli@gmail.com>
2026-02-02 12:45:58 -0300

    feat: add setup_logger function for application logging configuration

---
+++ logger/setup_logger.py
@@ -0,0 +1,9 @@
+import logging
+
+def setup_logger():
+    """Set up the logger for the application."""
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s [%(levelname)s] %(name)s: %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
